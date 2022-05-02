f60b3de1096c06695ef53b43a896b0dcb516106e
gideonokyere <okyeregideon10@gmail.com>
2022-05-02 20:53:28 +0000

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,30 @@
-import Express,{Request,Response} from 'express';
-const app = Express();
+import 'reflect-metadata';
+import dotenv from 'dotenv';
+dotenv.config();
+import { buildSchema } from 'type-graphql';
+import {ApolloServer} from 'apollo-server';
+import UserResolver from './resolvers/user.resolver';
+import { VerifyToken} from './controllers/auth.controller';
 
 const PORT = process.env.PORT || 8080;
 
-app.get('/',(req:Request,res:Response)=>{
-    res.status(200).send('Hello World');
-});
+async function bootstrap() {
+    const schema = await buildSchema({
+        resolvers: [UserResolver],
+        authChecker:VerifyToken
+    });
 
-app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`));
+    const server = new ApolloServer({
+        schema,
+        context:({req,res})=>{
+            return {
+                req,
+                res
+            };
+        }
+    });
+
+    await server.listen(PORT,()=>console.log(`Server is running on port:${PORT}`));
+}
+
+bootstrap();
